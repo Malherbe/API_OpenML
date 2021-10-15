361298248fdeee8c7340af3e758972bdbc0d8d0d
Malherbe <malherbe.billy@gmail.com>
2021-10-15 20:58:09 +0800

    correction"

--- lib/data_set_info.rb
+++ lib/data_set_info.rb
@@ -16,18 +16,26 @@ end
 oml_response = {}
 oml_results = {}
 
+
+## HAPPY DATA INFO REQUEST
 data_info_url = oml_api_path('data/1', config)
 oml_response[data_info_url] = call_oml_url(data_info_url)
-data_info = oml_response[data_info_url]
+data_info = JSON.parse(oml_response[data_info_url].body.to_s)
 
-oml_results['description'] = data_info['description']
+oml_results['name'] = data_info['data_set_description']['name']
 
-oml_results['name'] = data_info['name']
+oml_results['description'] = data_info['data_set_description']['description']
 
-oml_results['format'] = data_info['format']
+oml_results['format'] = data_info['data_set_description']['format']
 
-oml_results['tag'] = data_info['tag']
+oml_results['tag'] = data_info['data_set_description']['tag']
 
-oml_results['original_data_url'] = data_info['original_data_url']
+oml_results['original_data_url'] = data_info['data_set_description']['original_data_url']
+
+
+## BAD DATA INFO REQUEST
+bad_data_info_url = oml_api_path('data/99999999', config)
+oml_response[bad_data_info_url] = call_oml_url(bad_data_info_url)
+JSON.parse(oml_response[data_info_url].body.to_s)
 
 File.write('spec/fixtures/oml_results.yml', oml_results.to_yaml)
